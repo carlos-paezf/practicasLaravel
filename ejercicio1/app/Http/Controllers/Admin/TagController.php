5c853e9b0d6cb495e6237d30aedff3ced29b839d
carlos-paezf <carlos.paezf@usantoto.edu.co>
2019-12-11 11:35:42 -0500

    Decimo intento

--- ejercicio1/app/Http/Controllers/Admin/TagController.php
+++ ejercicio1/app/Http/Controllers/Admin/TagController.php
@@ -34,7 +34,7 @@ class TagController extends Controller
      */
     public function create()
     {
-        return view('admin.tags,create');
+        return view('admin.tags.create');
     }
 
     /**
